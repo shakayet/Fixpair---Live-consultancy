4c9ee150761dab349ec114e4052f7bf7b492554e
Shakhawat <srabonshakhawat@gmail.com>
2026-04-30 04:38:28 +0600

    fix type error

--- src/app/modules/consultation/consultation.service.ts
+++ src/app/modules/consultation/consultation.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { StatusCodes } from 'http-status-codes';
 import { JwtPayload } from 'jsonwebtoken';
 import mongoose from 'mongoose';
